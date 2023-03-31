8150adb7ccdd9d0b336ff0a51621bdc5c5675e49
BalthazarBujard <balthazar.bujard@gmail.com>
2023-03-31 05:32:37 -0400

    lidar_nav reel

--- src_lidar/lidar_data_reel.py
+++ src_lidar/lidar_data_reel.py
@@ -18,10 +18,39 @@ class Control():
 #We're listenning to the /LidarScan topic, processing the data and publishing to /front_data and /side_data
 
 def interpolate(data, ind):
+
+    for i in range(len(data)): #on interpole 1x par scan
+        if data[i]==float('inf'):
     
-    s,e=ind,ind
-    #rospy.loginfo(f"ind={i}")
-    #on cherche debut
+            s,e=i,i
+            #rospy.loginfo(f"ind={i}")
+            #on cherche debut
+            while data[s]==float('inf'):
+                s-=1
+
+            #if s==-1:s=0
+            #chercher fin du trou   
+            while e<=len(data)-1 and data[e]==float('inf'):
+                e+=1
+
+            if e==len(data):e=len(data)-1
+
+            dx=e-s
+            dy=data[e]-data[s]
+            x0,y0=s,data[s]
+
+            #print(s,e,dx,dy)
+            #interpolation du trou
+            if dx!=0: #safety
+                for j in range(s+1,e):
+                    
+                    data[j]=data[s] + dy/dx * (j-s)
+            
+            i=e
+
+    """  s,e=ind,ind
+        #rospy.loginfo(f"ind={i}")
+        #on cherche debut
     while data[s]==float('inf'):
         s-=1
 
@@ -30,8 +59,7 @@ def interpolate(data, ind):
     while e<=len(data)-1 and data[e]==float('inf'):
         e+=1
 
-    if e==len(data):e=len(data)-1
-
+    if e==len(data):e=len(data)-1 """
     #FAIRE ATTENTIO DES FOIS INTERPOLATION COMPLETE DES TROUS QUI SONT VRM PRESENTS DANS LE CIRCUIT
     #PAS CRITIQUE MAIS FAUT FAIRE ATTENTION-> CONDITION DE SECURITE 
     # -> P.EX. SI LA PENTE DY/DX EST TROP GRANDE => PROBABLEMENT VRAI TROU
@@ -39,7 +67,7 @@ def interpolate(data, ind):
 
     #construction droite lineaire
     #print(s,e,data[s],data[e])
-    dx=e-s
+    """ dx=e-s
     dy=data[e]-data[s]
     x0,y0=s,data[s]
 
@@ -48,38 +76,46 @@ def interpolate(data, ind):
     if dx!=0: #safety
         for j in range(s+1,e):
             
-            data[j]=data[s] + dy/dx * (j-s)
+            data[j]=data[s] + dy/dx * (j-s) """
     
     return data #interpolated data with no holes
 
 
 def lidar_preprocess_callback(msg,c):
      
-    scan=np.array(msg.ranges)
+    scan=np.array(msg.ranges) #scan reel a devant a 0 donc on shift tous l'array de n/2 pour garder les autres noeuds focntionnels
     n=len(scan)
-    
-    intv=[int(rospy.get_param("angle0",default=120)/90 * n/4),
+    scan=np.roll(scan,-n//2)#on shift l'array pour avoir devant a n//2
+    scan=np.flip(scan)#on inverse l'ordre -> gauche droite
+
+    intv=[int((rospy.get_param("angle0",default=120))/90 * n/4),
          int(rospy.get_param("angle1",default=240)/270 * 3*n/4)] #angles interval
     
     
     angles=np.linspace(0,2*np.pi,n)
     c.front_angles=angles[intv[0]:intv[1]] #on va pas regarder tout le cadran avant mais, a nouveau, un sous ensmeble
-    c.front_dist.data=[] #remise a zero du batch
+    #c.front_dist.data=[] #remise a zero du batch
     #utiliser que cadran avant du lidar -> [pi/2, 3*pi/2] = [len(scan)//4, 3*len(scan)//4]
 
     #PAS BESOIN DE X ET Y -> COORDONNEES POLAIRES SONT PLUS ADAPTEES
     #GARDER QUE UN SOUS ENSMEBLE POUR ACCELERER LE PROCESSUS
+    c.front_dist.data=np.zeros(len(c.front_angles))
+    iter=0
+    scan=interpolate(scan,0)
+    #now=rospy.get_rostime().nsecs
     for i in range(n)[intv[0]:intv[1]]:
         if scan[i]!=float('inf'):
-            c.front_dist.data.append(np.clip(scan[i],0,3)) #on clip pour s'orienter par rapport a l'env "local"
+            c.front_dist.data[i-intv[0]]=np.clip(scan[i],0,3) #on clip pour s'orienter par rapport a l'env "local"
         
         else:
             #chercher la bonne valeur a prendre
             #on a inf pour certaines sections du scan -> on va interpoler lineairement (approx) pour completer ces points
-            scan=interpolate(scan,i)
             
-            c.front_dist.data.append(np.clip(scan[i],0,3))
-    
+            scan=interpolate(scan,i)
+            iter+=1
+            print(iter)
+            c.front_dist.data[i-intv[0]]=(np.clip(scan[i],0,3))
+    #print(rospy.get_rostime().nsecs-now)
     #side data pour rester au milieu
     c.side_dist.data=[0,0]
     for ind,i in enumerate([n//4,3*n//4]):
@@ -121,7 +157,7 @@ if __name__=='__main__':
         side_topic=rospy.get_param("~side_topic",default="/side_data") 
         side_pub=rospy.Publisher(side_topic,Float32MultiArray,queue_size=10)
 
-        rate=rospy.Rate(2)
+        rate=rospy.Rate(10)
         #COMMENT BIEN SYCHRONISER LA PUBLICATION? -> LIMITE PAR /LidarScan -> pas besoin de rate.sleep?
 
         while not rospy.is_shutdown():
