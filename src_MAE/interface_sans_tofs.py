50961d2d9412e73c4d6bb8d5a02c886e517c5a27
JJoyet <julien.joyet@etu.sorbonne-universite.fr>
2023-04-25 11:00:45 +0200

    update simu

--- src_MAE/interface_sans_tofs.py
+++ src_MAE/interface_sans_tofs.py
@@ -11,31 +11,50 @@ import rospy
 from std_msgs.msg import Float32MultiArray, Float32, Bool, Int8
 
 class Navigation() : 
+    """Ce noeud a pour but de faire la transition entre les différents noeuds de navigation et les commandes données au robot,
+        les commandes données au robot vont dépendre de l'état donnée par la MAE:
+        -Etat 0: Navigation lidar
+        -Etat 1: Marche_arrière
+        
+    Ce noeud d'interface prend en entrée:
+        /LidarSpeedAngleCommand
+        /MarcheArriereSpeedAngleCommand
+        /State
+        
+    Les sorties de ce noeud d'interface sont:
+        /SpeedCommand
+        /AngleCommand
+        
+    Les entrées sont les commandes envoyé des différents noeuds de navigation et les sorties sont des commandes pour le robots choisi parmis les différentes navigations en fonction de l'état"""
+
 
     def __init__(self) :
-
-        # variables
-        self.speed = 0.0
-        self.angle = 0.0
-        self.marche_arr = {"speed" : 0.0, "angle" : 0.0}
-        self.nav_lidar = {"speed" : 0.0, "angle" : 0.0}
-        self.EP=0
-        self.start=False
-
         # Init ROS node
         rospy.init_node('navigation_haut_niveau', anonymous=True)
 
-        # Init ROS publishers
+            #======Attributs======
+        #Commandes par default
+        self.speed = 0.0
+        self.angle = 0.0
+        #Initialisations des attributs qui vont prendre les valeurs de commandes des différentes navigation
+        self.marche_arr = {"speed" : 0.0, "angle" : 0.0}
+        self.nav_lidar = {"speed" : 0.0, "angle" : 0.0}
+        #Etat présent
+        self.EP=0
+        #Variable d'activation de la navigation du robot
+        self.start=False
+
+            #======Publishers======
         self.pub_speed = rospy.Publisher("/SpeedCommand", Float32, queue_size = 1)
         self.pub_angle = rospy.Publisher("/AngleCommand", Float32, queue_size = 1)
 
-        # Init ROS subscribers
+            #======Subscribers======
         self.sub_marche_arr = rospy.Subscriber("/MarcheArriereSpeedAngleCommand", Float32MultiArray, self.callback_march_arr)
         self.sub_nav_lidar = rospy.Subscriber("/LidarSpeedAngleCommand", Float32MultiArray, self.callback_lidar)
         self.sub_state = rospy.Subscriber("/State", Int8, self.callback_state)
-        
-        # other subscribers should be added here
         self.sub_start = rospy.Subscriber("/Start_flag",Bool,self.start_stop_callback) #start/stop flag control from teleop_robot.py
+
+# CALLBACKS ==============================================================================================================
 
     def start_stop_callback(self,msg):
         self.start=msg.data
@@ -54,6 +73,8 @@ class Navigation() :
          """ Callback for the robot state"""
          self.EP=msg.data
 
+# PUBLICATION DES COMMANDES DU ROBOT ==============================================================================================================
+
     def set_speed_angle(self, speed, angle) : 
         """ Set the speed and the angle of the car"""
         self.speed = speed
@@ -61,21 +82,19 @@ class Navigation() :
         self.pub_speed.publish(self.speed)
         self.pub_angle.publish(self.angle)
 
+# Algo ==============================================================================================================
 
     def run(self) :
         """ Main loop of the navigation"""
-
-        # constants
-
 
         # frequency of the loop
         HZ = 20 # Hz
         rate = rospy.Rate(HZ)
         nav=""
+
         # main loop
         while not rospy.is_shutdown() :
             #En fonction de l'état on choisi quel commande de vitesse utiliser
-
             if self.start==True:
                 if self.EP == 0:
                     if nav!="lidar":
@@ -91,14 +110,10 @@ class Navigation() :
             else:
                 self.set_speed_angle(0, 0)
 
-                
-            
             rate.sleep()
         
-
 if __name__ == "__main__" :
     
         nav = Navigation()
         nav.run()
-
         rospy.spin()
